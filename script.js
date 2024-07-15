165d99112fb25754be5116dce840f874d68493bb
marcella.trindade <tchella12trindade@gmail.com>
2024-07-15 08:09:01 -0300

    code update

---
+++ script.js
@@ -0,0 +1,24 @@
+// js/script.js
+function loadComponent(elementId, componentPath, cssPath) {
+    fetch(componentPath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+            if (cssPath) {
+                loadCSS(cssPath);
+            }
+        })
+        .catch(error => console.error('Error loading component:', error));
+}
+
+function loadCSS(cssPath) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = cssPath;
+    document.head.appendChild(link);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadComponent("navbar", "components/navbar/navbar.html", "components/navbar/navbar.css", "/style.css");
+    loadComponent("header", "components/header/header.html", "components/header/header.css", "/style.css");
+});
